cf0dad1ae7175d2d987909fefe547c6d2ae59bf1
Glam52 <gla_max@outlook.com>
2024-04-20 21:59:48 +0700

    Работа с выводом справки

---
+++ gendiff/scripts/generate_diff.py
@@ -0,0 +1,15 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(prog='gendiff')
+
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+
+    parser.add_argument('-f', '--format', dest='format', help='set format of output')
+    parser.parse_args(['-h'])
+    parser.parse_args(['-f FORMAT'])
+
+
+if __name__ == '__main__':
+    main()
