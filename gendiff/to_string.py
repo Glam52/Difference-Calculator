ddbd1af46a08a5d4a97eb2312c902fbb2081f9ad
Glam52 <gla_max@outlook.com>
2024-04-26 18:57:34 +0700

    init makefile and workflow

--- gendiff/to_string.py
+++ gendiff/to_string.py
@@ -7,7 +7,7 @@ def to_text(compared_result):
     for item in compared_result:
         key = item['key']
         if item['type'] == 'untouched':
-            text += f"  {key}: {item['value']}\n"
+            text += f"    {key}: {item['value']}\n"
         elif item['type'] == 'modified':
             text += f"  - {key}: {item['value1']}\n"
             text += f"  + {key}: {item['value2']}\n"
