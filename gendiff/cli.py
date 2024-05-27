f24eec71701c93428e88557b846e01c1260d1ee2
Glam52 <gla_max@outlook.com>
2024-05-27 21:27:08 +0700

    work with lint

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -14,14 +14,22 @@ from gendiff.diff_run import gendiff
 #     parser.parse_args(["-h"])
 #     parser.parse_args(["-f FORMAT"])
 def main():
-    parser = argparse.ArgumentParser(description='usage: gendiff [-h] first_file second_file')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', choices=['stylish', 'json', 'plain'], default='stylish')
+    parser = argparse.ArgumentParser(
+        description="usage: gendiff [-h] first_file second_file"
+    )
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+    parser.add_argument(
+        "-f",
+        "--format",
+        choices=["stylish", "json", "plain"],
+        default="stylish",
+    )
     args = parser.parse_args()
 
     result = gendiff(args.first_file, args.second_file, args.format)
     print(result)
+
 
 if __name__ == "__main__":
     main()
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -5,40 +5,49 @@ import os
 def test_stylish_json():
     file1 = os.path.join(os.path.dirname(__file__), "fixtures", "file1.json")
     file2 = os.path.join(os.path.dirname(__file__), "fixtures", "file2.json")
-    final_file = open(os.path.join(os.path.dirname(__file__), "fixtures", "result_stylish"), "r").read()
+    final_file = open(
+        os.path.join(os.path.dirname(__file__), "fixtures", "result_stylish"),
+        "r",
+    ).read()
 
     result = gendiff(file1, file2)
     with open("test_st", "w") as file:
         file.write(result)
-    assert  result == final_file
+    assert result == final_file
 
 
 def test_stylish_yaml():
     file1 = os.path.join(os.path.dirname(__file__), "fixtures", "file1.yaml")
     file2 = os.path.join(os.path.dirname(__file__), "fixtures", "file2.yaml")
-    final_file = open(os.path.join(os.path.dirname(__file__), "fixtures", "result_yaml"), "r").read()
+    final_file = open(
+        os.path.join(os.path.dirname(__file__), "fixtures", "result_yaml"), "r"
+    ).read()
 
     result = gendiff(file1, file2, "stylish")
     with open("test_st", "w") as file:
         file.write(result)
-    assert  result == final_file
+    assert result == final_file
 
 
 def test_plain():
     file1 = os.path.join(os.path.dirname(__file__), "fixtures", "file1.json")
     file2 = os.path.join(os.path.dirname(__file__), "fixtures", "file2.json")
-    final_file = open(os.path.join(os.path.dirname(__file__), "fixtures", "result_plain"), "r").read()
+    final_file = open(
+        os.path.join(os.path.dirname(__file__), "fixtures", "result_plain"), "r"
+    ).read()
     result = gendiff(file1, file2, "plain")
     with open("test_st", "w") as file:
         file.write(result)
-    assert  result == final_file
+    assert result == final_file
 
 
 def test_json():
     file1 = os.path.join(os.path.dirname(__file__), "fixtures", "file1.json")
     file2 = os.path.join(os.path.dirname(__file__), "fixtures", "file2.json")
-    final_file = open(os.path.join(os.path.dirname(__file__), "fixtures", "result_json"), "r").read()
+    final_file = open(
+        os.path.join(os.path.dirname(__file__), "fixtures", "result_json"), "r"
+    ).read()
     result = gendiff(file1, file2, "json")
     with open("test_st", "w") as file:
         file.write(result)
-    assert  result == final_file
+    assert result == final_file
