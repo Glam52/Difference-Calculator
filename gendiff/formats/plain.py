71117a6a1fdb1168821adc0c82bc7e7b1f49011e
Glam52 <gla_max@outlook.com>
2024-05-28 13:07:30 +0700

    error correction test_generate_diff[yml]

--- gendiff/formats/plain.py
+++ gendiff/formats/plain.py
@@ -4,11 +4,11 @@ def format_value(value):
     elif isinstance(value, bool):
         return str(value).lower()
     elif isinstance(value, str):
+        if value == "0":
+            return "None"
         return f"'{value}'"
     elif value is None:
         return "null"
-    elif value == 0:
-        return None
 
 
 def plain(compared_result: list[dict], parent_key: str = "") -> str:
@@ -28,16 +28,10 @@ def plain(compared_result: list[dict], parent_key: str = "") -> str:
         elif item.get("type") == "modified":
             value1 = item.get("value1")
             value2 = item.get("value2")
-            formatted_value1 = format_value(value1)
-            formatted_value2 = format_value(value2)
-            if formatted_value1 is None:
-                formatted_value1 = "None"
-            if formatted_value2 is None:
-                formatted_value2 = "None"
             result += (
                 f"Property '{full_key}'"
-                f" was updated. From {formatted_value1}"
-                f" to {formatted_value2}\n"
+                f" was updated. From {format_value(value1)}"
+                f" to {format_value(value2)}\n"
             )
         elif item.get("type") == "nested":
             result += plain(item.get("children"), full_key)
