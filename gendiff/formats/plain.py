541031b6286e2763b84032863f7c3f0f5e5cb904
Glam52 <gla_max@outlook.com>
2024-05-28 18:30:56 +0700

    error correction test_generate_diff[yml]

--- gendiff/formats/plain.py
+++ gendiff/formats/plain.py
@@ -13,28 +13,29 @@ def format_value(value):
 
 
 def plain(compared_result: list[dict], parent_key: str = "") -> str:
-    result = ""
-    for item in compared_result:
+    result = []
+    for i, item in enumerate(compared_result):
         key = item.get("key")
         full_key = f"{parent_key}.{key}" if parent_key else key
 
         if item.get("type") == "new":
             value = item.get("value")
-            result += (
-                f"\nProperty '{full_key}'"
+            result.append(
+                f"Property '{full_key}'"
                 f" was added with value: {format_value(value)}"
             )
         elif item.get("type") == "deleted":
-            result += f"\nProperty '{full_key}' was removed"
+            result.append(f"Property '{full_key}' was removed")
         elif item.get("type") == "modified":
             value1 = item.get("value1")
             value2 = item.get("value2")
-            result += (
-                f"\nProperty '{full_key}'"
+            result.append(
+                f"Property '{full_key}'"
                 f" was updated. From {format_value(value1)}"
                 f" to {format_value(value2)}"
             )
         elif item.get("type") == "nested":
-            result += plain(item.get("children"), full_key)
+            result.append(plain(item.get("children"), full_key))
 
-    return result
+    return "\n".join(result)
+
